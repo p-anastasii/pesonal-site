332fce7ee5f0dffcd3caa98e1ed54ac571c542ac
p-anastasii <aportnenko98@gmail.com>
2024-11-12 16:05:24 -0500

    Add H/W Lesson 8

--- Desktop/Hillel/Test-JS/scripts/scripts.js
+++ Desktop/Hillel/Test-JS/scripts/scripts.js
@@ -311,3 +311,46 @@ function enterNumber(){
     }
     console.log("Maximum number of input attempts reached. Your last entered number:", numberInput);
 }
+
+
+//Lesson 8
+//Створити ladder (сходи) – об'єкт, який дозволяє підніматися вгору та спускатися:
+
+// let ladder = {
+//   up: function () { // підніматиме вас на одну сходинку
+//   },
+//   down: function () { // опускатиме вас на одну сходинку
+//   },
+//   showStep: function () { // показує поточну сходинку
+//   }
+//};
+
+// Тепер, якщо нам потрібно зробити кілька послідовних викликів, ми можемо виконати це так:
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+// Змініть код методів up, down і showStep таким Таким чином, щоб їх виклик можна було зробити по ланцюжку, наприклад:
+// ladder.up().up().down().showStep(); // 1
+
+
+let ladder = {
+    step: 0,
+    up: function () {
+      this.step++;
+      return(this)
+    },
+    down: function () {
+      this.step -= 1;
+      return(this)
+    },
+    showStep: function () {
+      console.log(this.step);
+      return(this);
+    }
+};
+ladder.up().up().down().showStep();
+
+
+
+
